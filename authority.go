8656fd81b894c905b19764bede8a4bef4fe18d76
hzakher <elhouri.hani@gmail.com>
2021-12-11 15:12:18 +0100

    code refactor

--- authority.go
+++ authority.go
@@ -2,6 +2,7 @@ package authority
 
 import (
 	"errors"
+
 	"gorm.io/gorm"
 )
 
@@ -14,6 +15,52 @@ type Authority struct {
 type Options struct {
 	TablesPrefix string
 	DB           *gorm.DB
+}
+
+// Permission represents the database model of permissions
+type Permission struct {
+	ID   uint
+	Name string
+}
+
+// TableName sets the table name
+func (p Permission) TableName() string {
+	return tablePrefix + "permissions"
+}
+
+// RolePermission stores the relationship between roles and permissions
+type RolePermission struct {
+	ID           uint
+	RoleID       uint
+	PermissionID uint
+}
+
+// TableName sets the table name
+func (r RolePermission) TableName() string {
+	return tablePrefix + "role_permissions"
+}
+
+// Role represents the database model of roles
+type Role struct {
+	ID   uint
+	Name string
+}
+
+// TableName sets the table name
+func (r Role) TableName() string {
+	return tablePrefix + "roles"
+}
+
+// UserRole represents the relationship between users and roles
+type UserRole struct {
+	ID     uint
+	UserID uint
+	RoleID uint
+}
+
+// TableName sets the table name
+func (u UserRole) TableName() string {
+	return tablePrefix + "user_roles"
 }
 
 var (
